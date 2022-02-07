1fff2de1225efe526dfaef142c6b60bb0711bba1
Mathijs Bakker <mathijs_bakker@me.com>
2022-02-07 17:37:07 +0100

    Import/use libs, Main app ceator (window builder)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,44 @@
+use fyrox::{
+    core::{
+        algebra::{UnitQuaternion, Vector3},
+        pool::Handle,
+    },
+    engine::{resource_manager, Engine},
+    event::{DeviceEvent, ElementState, Event, VirtualKeyCode, WindowEvent},
+    event_loop::{ControlFlow, EventLoop},
+    gui::window::WindowBuilder,
+    resource::texture::TextureWrapMode,
+    scene::{
+        base::BaseBuilder,
+        camera::{CameraBuilder, SkyBox, SkyBoxBuilder},
+        collider::{ColliderBuilder, ColliderShape},
+        node::Node,
+        rigidbody::RigidBodyBuilder,
+        transform::TransformBuilder,
+        Scene,
+    },
+    window::WindowBuilder,
+};
+use std::time;
+
+// Update game logic rate in Hz.
+const TIMESTEP: f32 = 1.0 / 60.0;
+
+struct Game {}
+
+impl Game {
+    pub fn new() -> Self {
+        Self {}
+    }
+
+    pub fn update(&mut self) {
+        // Todo: Gamelogic
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    // 1.a - Configure app window:
+    let window_builder = WindowBuilder::new().with_title("Rusty Shooter");
+    // 1.b - OS event listener:
+    let event_loop = EventLoop::new();
 }
