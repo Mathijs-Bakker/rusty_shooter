97ea6ea46225921056368f39e48a955af3b08819
Mathijs Bakker <mathijs_bakker@me.com>
2022-02-08 00:30:04 +0100

    Gameloop

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,8 @@ use fyrox::{
         pool::Handle,
     },
     engine::{resource_manager, Engine},
-    event::{DeviceEvent, ElementState, Event, VirtualKeyCode, WindowEvent},
+    event::{Event, VirtualKeyCode, WindowEvent},
+    // window::WindowBuilder,
     event_loop::{ControlFlow, EventLoop},
     gui::window::{WindowBuilder, WindowTitle},
     resource::texture::TextureWrapMode,
@@ -17,7 +18,6 @@ use fyrox::{
         transform::TransformBuilder,
         Scene,
     },
-    window::WindowBuilder,
 };
 use std::time;
 
@@ -38,8 +38,66 @@ impl Game {
 
 fn main() {
     // 1.a - Configure app window:
-    let window_builder = WindowBuilder::new(fyrox::gui::widget::WidgetBuilder::new())
-        .with_title(WindowTitle::Text("Rusty Shooter".to_string()));
+    let window_builder =
+        fyrox::window::WindowBuilder::new().with_title("Rusty Shooter".to_string());
     // 1.b - OS event listener:
     let event_loop = EventLoop::new();
+
+    // Create an instance of the engine.
+    let mut engine = Engine::new(window_builder, &event_loop, false).unwrap();
+
+    // Initialize game instance.
+    // Todo: It is empty for now.
+    let mut game = Game::new();
+
+    // Run the event loop of the main window. which will respond to OS and window events and update
+    // engine's state accordingly. Engine lets you to decide which event should be handled,
+    // this is a minimal working example of how it should be.
+    let clock = time::Instant::now();
+
+    let mut elapsed_time = 0.0;
+    event_loop.run(move |event, _, control_flow| {
+        match event {
+            Event::MainEventsCleared => {
+                // This main game loop - it has fixed time step which means that game
+                // code will run at fixed speed even if renderer can't give you desired
+                // 60 fps.
+                let mut dt = clock.elapsed().as_secs_f32() - elapsed_time;
+                while dt >= TIMESTEP {
+                    dt -= TIMESTEP;
+                    elapsed_time += TIMESTEP;
+
+                    // Run our game's logic.
+                    game.update();
+
+                    // Update engine each frame.
+                    engine.update(TIMESTEP);
+                }
+
+                // Rendering must be explicitly requested and handled after RedrawRequested event is received.
+                engine.get_window().request_redraw();
+            }
+            Event::RedrawRequested(_) => {
+                // Render at max speed - it is not tied to the game code.
+                engine.render().unwrap();
+            }
+            Event::WindowEvent { event, .. } => match event {
+                WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
+                WindowEvent::KeyboardInput { input, .. } => {
+                    // Exit game by hitting Escape.
+                    if let Some(VirtualKeyCode::Escape) = input.virtual_keycode {
+                        *control_flow = ControlFlow::Exit
+                    }
+                }
+                WindowEvent::Resized(size) => {
+                    // It is very important to handle Resized event from window, because
+                    // renderer knows nothing about window size - it must be notified
+                    // directly when window size has changed.
+                    engine.set_frame_size(size.into()).unwrap();
+                }
+                _ => (),
+            },
+            _ => *control_flow = ControlFlow::Poll,
+        }
+    });
 }
